540dad68c5dbe767c995a8cc1e053b8e234db1ba
Alan Marazzi <alan.marazzi@mosaicoon.com>
2016-11-04 17:22:01 +0100

    update of the logistic model and code cleaning

--- titanic.R
+++ titanic.R
@@ -5,13 +5,17 @@ trees_packages <- c(
     "FFTrees",
     "evtree",
     "party",
-    "randomForest"
+    "randomForest",
+    "intubate"
 )
 install.packages(trees_packages)
 
 # Get the data (folder "data" in this repo) and load it. 
 # I always avoid to convert all strings to factors
-titanic <- read.csv("data/train.csv", stringsAsFactors = FALSE, na.strings = "")
+titanic <- read.csv(
+    "https://raw.githubusercontent.com/alanmarazzi/trees-forest/master/data/train.csv", 
+    stringsAsFactors = FALSE, 
+    na.strings = "")
 
 # Take a look at the structure of the dataset
 str(titanic)
@@ -22,35 +26,30 @@ names(titanic) <- tolower(names(titanic))
 # sex and embarked to factors
 titanic$sex <- as.factor(titanic$sex)
 
-# By checking embarked we see that there are some "" empty characters
-# We want them as NA and not as a level
-# First check all unique values of embarked
-unique(titanic$embarked)
-
-# Assign NAs to "" empty character
-titanic$embarked[titanic$embarked == ""] <- NA
-
 # Now convert to factor
 titanic$embarked <- as.factor(titanic$embarked)
+
+# The age variable has some missing values
+mean(is.na(titanic$age))
 
 # Deal with NA in age variable
 age_prediction <- lm(age ~ survived + pclass + fare, data = titanic)
 summary(age_prediction)
-titanic$age[is.na(titanic$age)] <- predict(
-    age_prediction,
-    newdata = titanic[is.na(titanic$age),]
-    )
+titanic$age[is.na(titanic$age)] <- predict(age_prediction,
+    newdata = titanic[is.na(titanic$age),])
 
 # Check NAs in age
 sum(is.na(titanic$age))
 
 # Remove variables that clearly have nothing to do with our prediction setting
+# and run a logistic regression as a benchmark
 library(dplyr)
-titanic_mod <- titanic %>% 
-    select(survived, pclass, sex, age, sibsp, parch, fare)
+library(intubate)
 
-# Our benchmark will be a logistic regression
-logi <- glm(survived ~ . - fare - parch, data = titanic_mod, family = binomial)
+logi <- titanic %>% 
+    select(survived, pclass, sex, age, sibsp) %>% 
+    ntbt_glm(survived ~ ., family = binomial)
+
 summary(logi)
 
 # Predict on training and test set
@@ -60,4 +59,22 @@ survivors_logi[logi_pred > .5] <- 1
 
 # This is going to be our benchmark
 table(model = survivors_logi, real = titanic$survived)
+(480 + 250)/nrow(titanic)
+
+# Now on the test set for submission on Kaggle
+test <- read.csv("https://raw.githubusercontent.com/alanmarazzi/trees-forest/master/data/test.csv",
+    stringsAsFactors = FALSE,
+    na.strings = "")
+
+names(test) <- tolower(names(test))
+test$sex <- as.factor(test$sex)
+
+test_logi_pred <- predict(logi, test, type = "response")
+surv_test_logi <- data.frame(PassengerId = test$passengerid, 
+    Survived = rep(0, nrow(test)))
+
+surv_test_logi$Survived[test_logi_pred > .5] <- 1
+table(surv_test_logi$Survived)
+write.csv(surv_test_logi, "results/logi.csv", row.names = FALSE)
+# Result is 0.77512
 
