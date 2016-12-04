2de8eba11c41ca15855e3abbcc5ce9b62af2b9f7
Alan Marazzi <alan.marazzi@mosaicoon.com>
2016-12-04 18:53:18 +0100

    added primer and updated script

--- titanic.R
+++ titanic.R
@@ -13,8 +13,7 @@ install.packages(trees_packages)
 
 # Get the data (folder "data" in this repo) and load it. 
 # I always avoid to convert all strings to factors
-titanic <- read.csv(
-    "https://raw.githubusercontent.com/alanmarazzi/trees-forest/master/data/train.csv", 
+titanic <- read.csv("https://raw.githubusercontent.com/alanmarazzi/trees-forest/master/data/train.csv", 
     stringsAsFactors = FALSE, 
     na.strings = "")
 
@@ -24,18 +23,20 @@ str(titanic)
 # The first thing I like to do is to convert all columns names to lowercase
 names(titanic) <- tolower(names(titanic))
 
-# sex and embarked to factors
+# sex and embarked are actually factors, so let's convert them
 titanic$sex <- as.factor(titanic$sex)
-
-# Now convert to factor
 titanic$embarked <- as.factor(titanic$embarked)
 
-# The age variable has some missing values
+# The age variable has some missing values, x%
 mean(is.na(titanic$age))
 
-# Deal with NA in age variable
+# Deal with NAs in age variable by substituting them with a linear regression
 age_prediction <- lm(age ~ survived + pclass + fare, data = titanic)
+
+# Comment on summary
 summary(age_prediction)
+
+# Now substitute NAs in the dataset
 titanic$age[is.na(titanic$age)] <- predict(age_prediction,
     newdata = titanic[is.na(titanic$age),])
 
@@ -47,10 +48,12 @@ sum(is.na(titanic$age))
 library(dplyr)
 library(intubate)
 
+# Instead of creating a temp variable pipe the dataset up to the model
 logi <- titanic %>% 
     select(survived, pclass, sex, age, sibsp) %>% 
     ntbt_glm(survived ~ ., family = binomial)
 
+# Always check statistics
 summary(logi)
 
 # Predict on training and test set
@@ -58,18 +61,19 @@ logi_pred <- predict(logi, type = "response")
 survivors_logi <- rep(0, nrow(titanic))
 survivors_logi[logi_pred > .5] <- 1
 
-# This is going to be our benchmark
+# This is going to be our training benchmark
 table(model = survivors_logi, real = titanic$survived)
-(480 + 250)/nrow(titanic)
 
 # Now on the test set for submission on Kaggle
 test <- read.csv("https://raw.githubusercontent.com/alanmarazzi/trees-forest/master/data/test.csv",
     stringsAsFactors = FALSE,
     na.strings = "")
 
+# Remember to apply the safe transformations as for the training set
 names(test) <- tolower(names(test))
 test$sex <- as.factor(test$sex)
 
+# Prediction of the test set and write results to csv for submission
 test_logi_pred <- predict(logi, test, type = "response")
 surv_test_logi <- data.frame(PassengerId = test$passengerid, 
     Survived = rep(0, nrow(test)))
@@ -79,9 +83,9 @@ table(surv_test_logi$Survived)
 write.csv(surv_test_logi, "results/logi.csv", row.names = FALSE)
 # Result is 0.77512
 
+### Fast and Frugal Trees
 # Temporary copy to avoid masking from FFTrees package
 titanicc <- titanic
-
 library(FFTrees)
 titanic <- titanicc
 rm(titanicc)
@@ -93,7 +97,8 @@ fftitanic <- titanic %>%
 
 # Plotting of the best tree
 plot(fftitanic, 
-     main = "Titanic", decision.names = c("Not Survived", "Survived"))
+     main = "Titanic", 
+     decision.names = c("Not Survived", "Survived"))
 
 # Build a simple classifier out of the best tree
 ffpred <- ifelse(test$sex != "male", 1,
@@ -102,27 +107,36 @@ ffpred <- ifelse(test$sex != "male", 1,
                                ifelse(test$age >= 21.36, 0, 1))))
 
 
+# FFTree doesn't deal with NAs, I assign a 0 to them
 ffpred[is.na(ffpred)] <- 0
 ffpred <- data.frame(PassengerId = test$passengerid, Survived = ffpred)
 write.csv(ffpred, "results/fftree.csv", row.names = FALSE)
 # Result is 0.76555
 
+### Inferential trees
 library(party)
 
+# ctree model building
 partyTitanic <- titanic %>% 
     select(age, pclass, sex, sibsp, fare, survived) %>% 
     ntbt(ctree, as.factor(survived) ~ .)
 
-plot(partyTitanic)
+# Plot the resulting tree
+plot(partyTitanic, main = "Titanic prediction", type = "simple",
+     inner_panel = node_inner(partyTitanic, 
+                              pval = FALSE),
+     terminal_panel = node_terminal(partyTitanic,
+                                    abbreviate = TRUE,
+                                    digits = 1,
+                                    fill = "white"))
 
+# Prediction of training data
 train_party <- Predict(partyTitanic)
-
 table(tree = train_party, real = titanic$survived)
 
+# Prediction of the test set
 party_pred <- Predict(partyTitanic, newdata = test)
-
 party_pred <- as.numeric(party_pred) - 1
-
 party_pred <- data.frame(PassengerId = test$passengerid, 
                          Survived = party_pred)
 write.csv(party_pred, "results/party.csv", row.names = FALSE)
@@ -130,15 +144,22 @@ write.csv(party_pred, "results/party.csv", row.names = FALSE)
 
 # Bagging
 library(randomForest)
+
+# If you want the same result remember to set the same seed
 set.seed(123)
+
+# Bagging model building
 titanic_bag <- titanic %>% 
     select(survived, age, pclass, sex, sibsp, fare, parch) %>% 
     ntbt_randomForest(as.factor(survived) ~ ., mtry = 6)
 
+# Bagging and Random Forest don't deal with NAs
 test$age[is.na(test$age)] <- median(test$age, na.rm = TRUE)
-    
+
+# The usual test set prediction
 bag_pred <- predict(titanic_bag, test)
 
+# Check if there are NAs in prediction and substitute them
 sum(is.na(bag_pred))
 bag_pred[is.na(bag_pred)] <- 1
 bag_pred <- data.frame(PassengerId = test$passengerid, 
@@ -149,10 +170,13 @@ write.csv(bag_pred, "results/bagging.csv", row.names = FALSE)
 
 # RandomForest
 set.seed(456)
+
+# Random Forest model building
 titanic_rf <- titanic %>% 
     select(survived, age, pclass, sex, sibsp, fare, parch) %>% 
     ntbt_randomForest(as.factor(survived) ~ ., mtry = 3, n.trees = 5000)
 
+# Prediction
 rf_pred <- predict(titanic_rf, test)
 rf_pred[is.na(rf_pred)] <- 1
 rf_pred <- data.frame(PassengerId = test$passengerid, Survived = rf_pred, row.names = 1:length(rf_pred))
@@ -178,6 +202,8 @@ write.csv(rf_party_pred, "results/rf_party.csv", row.names = FALSE)
 # Boosting
 library(gbm)
 set.seed(999)
+
+# Boosting model building
 titanic_boost <- titanic %>% 
     select(survived, age, pclass, sex, sibsp, fare, parch) %>% 
     ntbt(gbm, survived ~ .,
